7396e67ec9fbdd89ddfa0717a4dd8656117f3f5d
James Wang <james_wangx@163.com>
2025-08-19 12:08:54 +0800

    Add getBOM by revRule

--- src/main/java/com/codicefun/tc/soa/util/StructureManagementUtil.java
+++ src/main/java/com/codicefun/tc/soa/util/StructureManagementUtil.java
@@ -1,7 +1,10 @@
 package com.codicefun.tc.soa.util;
 
 import com.codicefun.tc.soa.clientx.AppXSession;
+import com.codicefun.tc.soa.exception.SoaUtilException;
+import com.teamcenter.schemas.soa._2006_03.exceptions.ServiceException;
 import com.teamcenter.services.strong.cad.StructureManagementService;
+import com.teamcenter.services.strong.cad._2007_01.StructureManagement;
 import com.teamcenter.services.strong.cad._2007_01.StructureManagement.*;
 import com.teamcenter.services.strong.cad._2008_06.StructureManagement.SaveBOMWindowsResponse;
 import com.teamcenter.services.strong.cad._2019_06.StructureManagement.CreateWindowsInfo3;
@@ -57,6 +60,45 @@ public class StructureManagementUtil {
         return Optional.ofNullable(response.output[0]);
     }
 
+    public static Optional<RevisionRule> getRevisionRule(String revRuleName) {
+        try {
+            StructureManagement.GetRevisionRulesResponse response = smService.getRevisionRules();
+            ServiceUtil.catchPartialErrors(response.serviceData);
+            for (StructureManagement.RevisionRuleInfo revisionRuleInfo : response.output) {
+                RevisionRule revRule = revisionRuleInfo.revRule;
+                String objectName = ModelObjectUtil.getPropStringValue(revRule, "object_name")
+                                                   .orElseThrow(() -> new SoaUtilException(
+                                                           "Not found object_name property in revision rule"));
+                if (objectName.equals((revRuleName))) {
+                    return Optional.of(revRule);
+                }
+            }
+        } catch (ServiceException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return Optional.empty();
+    }
+
+    public static Optional<CreateBOMWindowsOutput> getBOM(ItemRevision itemRevision, String revRule) {
+        CreateWindowsInfo3[] inputs = new CreateWindowsInfo3[1];
+        inputs[0] = new CreateWindowsInfo3();
+        getPSBOMView(itemRevision).ifPresent(psbomView -> inputs[0].bomView = psbomView);
+        inputs[0].itemRev = itemRevision;
+        RevisionRuleConfigInfo revRuleConfigInfo = new RevisionRuleConfigInfo();
+        revRuleConfigInfo.revRule = getRevisionRule(revRule).orElseThrow(
+                () -> new SoaUtilException("Not found revision rule by name: " + revRule));
+        inputs[0].revRuleConfigInfo = revRuleConfigInfo;
+        CreateBOMWindowsResponse response = smService.createOrReConfigureBOMWindows(inputs);
+        if (ServiceUtil.catchPartialErrors(response.serviceData) ||
+            response.output == null ||
+            response.output.length == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(response.output[0]);
+    }
+
     /**
      * Send rev to structure manager and return the top line
      */
@@ -93,7 +135,7 @@ public class StructureManagementUtil {
         ExpandPSOneLevelPref expandPref = new ExpandPSOneLevelPref();
         expandPref.expItemRev = false;
         ExpandPSOneLevelOutput[] expandOutputs = smService.expandPSOneLevel(expendInfo,
-                expandPref).output;
+                                                                            expandPref).output;
 
         if (expandOutputs.length == 0) {
             return null;
